c165bd647fe2e305b39b5547b5e71ff629e989ad
Алексий <99173232+AlekseyWm@users.noreply.github.com>
2024-01-08 20:31:56 +0300

    45

---
+++ Lr4/modules/consts.js
@@ -0,0 +1,5 @@
+export const groupId = 224165811
+export const accessToken = 'vk1.a.Tjy-IsHn4Bqr7Pa2RO5Bhny3LrKMHEJP8JIeDUAmzpTuEBkdmaN8IvmbrDwM82cnB-2krUouPG6D4B8G6DhDQepjb7oHwojinDw1mqKdUx3NuXX4MCufxWArIrV9-EGlb9SLeYjLh7nEigrZAruehr-GLbfGo5XSxTQmGRYrcXdbSAy-OclItcofVvhpdJ35t-OSmunMuZ2EkDF6Wg23cQ'
+export const version = '5.199'
+export const peerId2 = 2000000002
+export const peerId1 = 2000000001
